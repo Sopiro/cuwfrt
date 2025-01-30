14ff066ead58d8739c18d5168d45fd1285d81193
sopiro <sopiro.ldh@gmail.com>
2025-01-30 22:49:26 +0900

    Test alzartak sample scene

--- src/main.cu
+++ src/main.cu
@@ -1,81 +1,156 @@
-#include <iostream>
-#include <math.h>
+#if defined(_WIN32)
+#include <crtdbg.h>
+#endif
 
-#include "api.h"
+#include "alzartak/batch_renderer.h"
+#include "alzartak/camera.h"
+#include "alzartak/window.h"
 
-__kernel__ void add(int n, float* x, float* y, float* z)
+using namespace alzartak;
+
+const float scale = 100.0f;
+float delta_time = 1.0f / 60.0f;
+
+Window* window;
+BatchRenderer* renderer;
+Camera2D* camera_2d;
+Camera3D* camera_3d;
+
+bool mode = true;
+
+void UpdateProjectionMatrix()
 {
-    uint32_t i = blockDim.x * blockIdx.x + threadIdx.x;
-    if (i < n)
+    Point2 extents = window->GetWindowSize();
+    extents /= scale;
+
+    WakNotUsed(extents);
+    if (mode)
     {
-        z[i] = x[i] + y[i];
+        Mat4 proj_matrix = Mat4::Orth(-extents.x / 2, extents.x / 2, -extents.y / 2, extents.y / 2, -1000, 1000);
+        renderer->SetProjectionMatrix(proj_matrix);
+    }
+    else
+    {
+        Mat4 proj_matrix = Mat4::Perspective(DegToRad(71.0f), 16.0f / 9.0f, 0.01f, 1000.0f);
+        renderer->SetProjectionMatrix(proj_matrix);
     }
 }
 
-int main(void)
+void Init()
 {
-    int N = 1 << 20; // 1,048,576
+    window = Window::Init(1280, 720, "alzartak");
+    renderer = new BatchRenderer;
 
-    float *x, *y, *z;
-    float *d_x, *d_y, *d_z;
+    camera_2d = new Camera2D;
+    camera_3d = new Camera3D;
 
-    // Allocate memory on host (CPU)
-    x = new float[N];
-    y = new float[N];
-    z = new float[N];
+    // Enable culling
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
 
-    // Allocate memory on device (GPU)
-    checkCudaErrors(cudaMalloc(&d_x, N * sizeof(float)));
-    checkCudaErrors(cudaMalloc(&d_y, N * sizeof(float)));
-    checkCudaErrors(cudaMalloc(&d_z, N * sizeof(float)));
+    // Enable blend
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    // Initialize x and y arrays on the host (CPU)
-    for (int i = 0; i < N; i++)
+    UpdateProjectionMatrix();
+    window->SetFramebufferSizeChangeCallback([&](int32 width, int32 height) -> void {
+        glViewport(0, 0, width, height);
+        UpdateProjectionMatrix();
+    });
+}
+
+void Terminate()
+{
+    delete camera_2d;
+    delete camera_3d;
+    delete renderer;
+}
+
+void Update()
+{
+    window->BeginFrame(Color::light_blue);
+    // ImGui::ShowDemoWindow();
+
+    ImGuiIO& io = ImGui::GetIO();
+
+    ImGui::SetNextWindowPos({ 4, 4 }, ImGuiCond_Once, { 0.0f, 0.0f });
+
+    if (ImGui::Begin("alzartak", NULL, ImGuiWindowFlags_AlwaysAutoResize))
     {
-        x[i] = 1.0f;
-        y[i] = 2.0f;
-        z[i] = 0;
+        ImGui::Text("%d fps", int32(io.Framerate));
+        if (ImGui::Checkbox("CameraMode 2D/3D", &mode))
+        {
+            UpdateProjectionMatrix();
+        }
+    }
+    ImGui::End();
+
+    // Camera control
+    {
+        if (mode)
+        {
+            camera_2d->UpdateInput(scale);
+            renderer->SetViewMatrix(camera_2d->GetCameraMatrix());
+        }
+        else
+        {
+            camera_3d->UpdateInput(delta_time);
+            renderer->SetViewMatrix(camera_3d->GetCameraMatrix());
+        }
     }
 
-    // Copy x and y arrays from host to device
-    checkCudaErrors(cudaMemcpy(d_x, x, N * sizeof(float), cudaMemcpyHostToDevice));
-    checkCudaErrors(cudaMemcpy(d_y, y, N * sizeof(float), cudaMemcpyHostToDevice));
+    // Rendering
+    renderer->SetPointSize(5);
+    renderer->SetLineWidth(2);
+    renderer->DrawLine({ 3, 0, -3 }, { 2, 1, -5 }, Vec4(1, 0, 1, 1));
+    renderer->DrawTriangle(
+        { { 0, 0, 0 }, Vec4(1, 0, 0, 1) }, { { 1, 0, -0.5f }, Vec4(0, 1, 0, 1) }, { { 0.5, 1.0, -1.0f }, Vec4(0, 0, 1, 1) }
+    );
+    renderer->DrawPoint({ -1, -1 }, Vec4(1, 0, 0, 1));
 
-    int threads = 256; // More optimal thread count for modern GPUs
-    int blocks = (N + threads - 1) / threads;
+    Vec3 m = Vec3(-2, 1, -1);
+    AABB a(m, m + Vec3(0.7f));
 
-    std::cout << blocks << ", " << threads << std::endl;
+    renderer->DrawAABB(a, Vec4(1, 1, 1, 0.5f));
 
-    // Run kernel on 1M elements on the GPU
-    add<<<blocks, threads>>>(N, d_x, d_y, d_z);
+    renderer->FlushAll();
 
-    // Check for kernel launch errors
-    checkCudaErrors(cudaGetLastError());
+    window->EndFrame();
+}
 
-    // Wait for GPU to finish before accessing on host
-    checkCudaErrors(cudaDeviceSynchronize());
+int main()
+{
+#if defined(_WIN32) && defined(_DEBUG)
+    // Enable memory-leak reports
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 
-    // Copy result from device to host
-    checkCudaErrors(cudaMemcpy(z, d_z, N * sizeof(float), cudaMemcpyDeviceToHost));
+    Init();
 
-    // Check for errors (all values should be 3.0f)
-    float max_error = 0.0f;
-    for (int i = 0; i < N; i++)
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(Update, 0, 1);
+#else
+    auto last_time = std::chrono::steady_clock::now();
+    const float target_frame_time = 1.0f / window->GetRefreshRate();
+
+    while (!window->ShouldClose())
     {
-        max_error = std::fmax(max_error, std::fabs(z[i] - 3.0f));
+        auto current_time = std::chrono::steady_clock::now();
+        std::chrono::duration<float> duration = current_time - last_time;
+        float elapsed_time = duration.count();
+        last_time = current_time;
+
+        delta_time += elapsed_time;
+        if (delta_time > target_frame_time)
+        {
+            Update();
+            delta_time -= target_frame_time;
+        }
     }
+#endif
 
-    std::cout << "Max error: " << max_error << std::endl;
-
-    // Free memory on device (GPU)
-    checkCudaErrors(cudaFree(d_x));
-    checkCudaErrors(cudaFree(d_y));
-    checkCudaErrors(cudaFree(d_z));
-
-    // Free memory on host (CPU)
-    delete[] x;
-    delete[] y;
-    delete[] z;
+    Terminate();
 
     return 0;
 }
