26bd08067c27cd7e82523317e2659adb02bd6b52
sopiro <sopiro.ldh@gmail.com>
2025-04-30 19:10:07 +0900

    Include texture sampling function

--- src/cuwfrt/kernel/device_texture.cuh
+++ src/cuwfrt/kernel/device_texture.cuh
@@ -14,4 +14,70 @@ inline __GPU__ Vec3 SampleTexture(const GPUScene* scene, TextureIndex ti, Point2
     return Vec3(tex_color.x, tex_color.y, tex_color.z);
 }
 
+enum TexCoordFilter
+{
+    repeat,
+    clamp,
+};
+
+inline __GPU__ void FilterTexCoord(int32* u, int32* v, int32 width, int32 height, TexCoordFilter texcoord_filter)
+{
+    switch (texcoord_filter)
+    {
+    case TexCoordFilter::repeat:
+    {
+        *u = *u >= 0 ? *u % width : width - (-(*u) % width) - 1;
+        *v = *v >= 0 ? *v % height : height - (-(*v) % height) - 1;
+    }
+    break;
+    case TexCoordFilter::clamp:
+    {
+        *u = Clamp(*u, 0, width - 1);
+        *v = Clamp(*v, 0, height - 1);
+    }
+    break;
+    default:
+        WakAssert(false);
+        break;
+    }
+}
+
+template <typename T>
+inline __GPU__ T SampleTexture(const T* image_texture, Point2i res, Point2 uv, TexCoordFilter texcoord_filter = repeat)
+{
+#if 0
+    // Nearest sampling
+    Float w = uv.x * image.width + 0.5f;
+    Float h = uv.y * image.height + 0.5f;
+
+    int32 i = int32(w);
+    int32 j = int32(h);
+
+    FilterTexCoord(&i, &j);
+
+    return image[i + j * image.width];
+#else
+    // Bilinear sampling
+    Float w = uv.x * image.width + 0.5f;
+    Float h = uv.y * image.height + 0.5f;
+
+    int32 i0 = int32(w), i1 = int32(w) + 1;
+    int32 j0 = int32(h), j1 = int32(h) + 1;
+
+    FilterTexCoord(&i0, &j0, image.width, image.height, texcoord_filter);
+    FilterTexCoord(&i1, &j1, image.width, image.height, texcoord_filter);
+
+    Float fu = w - std::floor(w);
+    Float fv = h - std::floor(h);
+
+    T v00 = image[i0 + j0 * image.width], v10 = image[i1 + j0 * image.width];
+    T v01 = image[i0 + j1 * image.width], v11 = image[i1 + j1 * image.width];
+
+    // clang-format off
+return (1-fu) * (1-fv) * v00 + (1-fu) * (fv) * v01 +
+       (  fu) * (1-fv) * v10 + (  fu) * (fv) * v11;
+    // clang-format on
+#endif
+}
+
 } // namespace cuwfrt
